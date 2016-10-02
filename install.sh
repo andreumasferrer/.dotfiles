92794247a1ee25ddcbe90858fb253d65df65bbd7
andreumasferrer <andreumasferrer@outlook.com>
2016-10-02 19:54:15 +0200

    Add atom config + install scripts

--- install.sh
+++ install.sh
@@ -5,11 +5,19 @@ export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
+echo ""
+echo "📥 Updating dotfiles..."
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
 echo ""
-echo "Creating symlinks..."
+echo "🔧 Creating symlinks..."
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/apps-conf/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/apps-conf/git/.gitignore_global" ~
+
+
+# Install software
+echo ""
+echo "💾 Installing base software..."
+. "$DOTFILES_DIR/install/base.sh"
