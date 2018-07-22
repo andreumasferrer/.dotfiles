bab705726467dbfdf4c9a2dec29b0bc5870e761a
Andreu Masferrer <amasferr@amasferr-mac.local>
2018-07-22 14:36:54 +0200

    Replace Atom with VSCode

--- install/base.sh
+++ install/base.sh
@@ -1,10 +1,3 @@
 #HomeBrew
 echo "➔ Installing HomeBrew, please wait..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-
-# Atom
-echo "➔ Installing Atom, please wait..."
-brew cask install atom
-echo "➔➔ Creating Atom config symlink"
-ln -svf "$DOTFILES_DIR/apps-conf/.atom/" ~/.atom
--- runcom/.bash_profile
+++ runcom/.bash_profile
@@ -10,3 +10,8 @@ unset DOTFILE
 
 # Export
 export DOTFILES_DIR
+
+# Node Version Manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
